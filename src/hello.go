a18a08fa314258f156e5dba983708653df6c4fa7
Macx-Roberto <macxizinhocesconetto@hotmail.com>
2020-12-13 15:47:15 -0300

    monitorar soudcloud

--- src/hello.go
+++ src/hello.go
@@ -8,19 +8,19 @@ import (
 
 func main() {
 	for {
-	exibeMenu()
-
-	switch leComando {
-	case 1:
-		iniciarMonitoramento()
-	case 2:
-		fmt.Println("Exibindo Logs...")
-	case 0:
-		fmt.Println("Saindo do programa")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço este comando")
-		os.Exit(-1)
+		exibeMenu()
+		switch leComando() {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
 	}
 }
 
@@ -34,14 +34,18 @@ func leComando() int {
 	var comandoLido int
 	fmt.Scan(&comandoLido)
 	fmt.Println("O comando escolhido foi", comandoLido)
-
 	return comandoLido
 }
 
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
-	site := "https://www.soudcloud.com.br"
+	site := "https://soundcloud.com/"
 	resp, _ := http.Get(site)
 	fmt.Println(resp)
 	// '_' serve para ignporar um dos retornos da funcao
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
 }
